ca89460b9223d1a88e1ac5a6d92a36e134c91dc7
Andrei <dandrei.web@gmail.com>
2023-05-12 16:42:39 +0300

    Work section center

--- src/components/MiddleSection/MiddleSection.tsx
+++ src/components/MiddleSection/MiddleSection.tsx
@@ -2,11 +2,11 @@ import { ParallaxLayer } from "@react-spring/parallax";
 import Work from "../Work/Work";
 
 const MiddleSection = () => {
-    return (
-        <ParallaxLayer offset={1} speed={0.1}>
-            <Work />
-        </ParallaxLayer>
-    );
+  return (
+    <ParallaxLayer className="flex" offset={1} speed={0.1}>
+      <Work />
+    </ParallaxLayer>
+  );
 };
 
 export default MiddleSection;
